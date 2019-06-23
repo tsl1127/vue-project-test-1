70920c60f26153fa5c5268b3366ad6c9ca51053c
tsl <taosiliang1127tsl@163.com>
2019-06-23 17:40:23 +0800

    icon iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,37 @@
 <template>
-        <button class="g-button">按钮</button>    
+        <button class="g-button" :class="{[`i-${iconPosition}`]:true}">
+                <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
+                <div class="content">
+                        <slot></slot>        
+                </div>
+        </button> 
 </template>
 <script>
 export default {
-
+        props:['icon', 'iconPosition']
 }
 </script>
 <style lang="scss" scoped>
         .g-button {
             font-size: var(--font-size);
             height: var(--button-height);
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            vertical-align: top;
+            > .icon {
+                    order:1;
+            }
+            > .content {
+                    order:2;
+            }
+            &.i-right {
+                >.icon {
+                        order:2;
+                }  
+                > .content {
+                        order:1;
+                }  
+            }
         }
 </style>
