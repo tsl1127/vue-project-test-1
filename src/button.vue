2f0be157e0b4691050478f5b696117fa7d18581b
tsl <taosiliang1127tsl@163.com>
2019-06-23 16:12:01 +0800

    使用parcel打包，将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,14 @@
+<template>
+        <button class="g-button">按钮</button>    
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+        .g-button {
+            font-size: var(--font-size);
+            height: var(--button-height);
+        }
+</style>
